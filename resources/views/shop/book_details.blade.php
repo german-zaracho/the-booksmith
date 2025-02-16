4dda439d9b2d4aa4ebead7c0be3ed6916d44e2e1
german-zaracho <germanzarachodev@gmail.com>
2025-02-16 14:25:34 -0300

    Adding some details to the books

--- resources/views/shop/book_details.blade.php
+++ resources/views/shop/book_details.blade.php
@@ -6,6 +6,8 @@
         <img src="{{ \Illuminate\Support\Facades\Storage::url($books->image) }}" alt="img" title="img" class="h-56 w-40 object-cover rounded-[20px] mt-3">
         <p class="text-black mt-3">Author: {{ $books->author }}</p>
         <p class="text-black mt-3 max-w-[600px]">Synopsis: {{ $books->synopsis }}</p>
+        <p class="text-black mt-3">Editorial: {{ $books->editorial }}</p>
+        <p class="text-black mt-3">Genre: {{ $books->genre->name }}</p>
         <p>Price: ${{ number_format($books->price * 100, 2) }}</p>
 
         <button class="mt-3 bg-blue-600 text-white rounded-md py-2 px-4 hover:bg-blue-700 transition duration-300 w-[100px]">Buy</button>
